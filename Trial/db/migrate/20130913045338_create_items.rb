77dc6cea252bf7d3d8caa2ebe0cc21db95228667
Eric <eric@ubuntu.ubuntu-domain>
2013-09-16 22:39:27 -0700

    Modified Items table data to be rails specific

--- Trial/db/migrate/20130913045338_create_items.rb
+++ Trial/db/migrate/20130913045338_create_items.rb
@@ -1,19 +1,19 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.bigint :id
-      t.varchar :name
-      t.longblob :image
-      t.varchar :description
-      t.int :hp
-      t.int :atk
-      t.int :def
-      t.int :spd
-      t.int :cost
-      t.tinyint :equip_flag
-      t.varchar :type
+      t.integer :id
+      t.string :name
+      t.binary :image
+      t.text :description
+      t.integer :hp
+      t.integer :atk
+      t.integer :def
+      t.integer :spd
+      t.integer :cost
+      t.boolean :equip_flag
+      t.string :type
       t.date :created_at
-      t.tinyint :maintenance
+      t.boolean :maintenance
 
       t.timestamps
     end

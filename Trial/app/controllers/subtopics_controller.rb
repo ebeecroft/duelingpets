5a4655995a7137c480a7f714b9dc270e18da76dc
Eric <eric@ubuntu.ubuntu-domain>
2013-09-30 12:45:33 -0700

    Working Maintopics and Subtopics, Issues still with Create and Update and Show links

--- Trial/app/controllers/subtopics_controller.rb
+++ Trial/app/controllers/subtopics_controller.rb
@@ -1,8 +1,10 @@
 class SubtopicsController < ApplicationController
   # GET /subtopics
   # GET /subtopics.json
+  #before_filter :load_forum
   def index
-    @maintopic.subtopics = Subtopic.all
+    @maintopic = Maintopic.find(params[:maintopic_id])
+    @subtopics = Subtopic.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,6 +15,7 @@ class SubtopicsController < ApplicationController
   # GET /subtopics/1
   # GET /subtopics/1.json
   def show
+    @maintopic = Maintopic.find(params[:maintopic_id])
     @subtopic = Subtopic.find(params[:id])
 
     respond_to do |format|
@@ -26,30 +29,41 @@ class SubtopicsController < ApplicationController
   def new
 #    @maintopic = Maintopic.find_by_id(params[:main_id])
 #    @subtopic = @maintopic.subtopics.build
-
 #    respond_to do |format|
 #      format.html # new.html.erb
 #      format.json { render json: @subtopic }
 #    end
+     @maintopic = Maintopic.find(params[:maintopic_id])
+     @subtopic = @maintopic.subtopics.build
   end
 
   # GET /subtopics/1/edit
   def edit
+    @maintopic = Maintopic.find(params[:maintopic_id])
     @subtopic = Subtopic.find(params[:id])
+    #@subtopic = @maintopic.subtopics.build
   end
 
   # POST /subtopics
   # POST /subtopics.json
   def create
-    @maintopic = Maintopic.find_by_id(params[:main_id])
-    @subtopic = @maintopic.subtopics.create(params[:subtopic])
-    @subtopic.save
-    redirect_to @maintopic.subtopic
+#    @maintopic = Maintopic.find_by_id(params[:main_id])
+     @maintopic = Maintopic.find(params[:maintopic_id])
+     @subtopic = @maintopic.subtopics.new(params[:subtopic])
+     @subtopic.save
+     #if @subtopic.save
+     #   redirect_to @maintopic.subtopic
+     #else
+     #   render "new";
+     #end
+     redirect_to @maintopic
   end
 
   # PUT /subtopics/1
   # PUT /subtopics/1.json
   def update
+    @maintopic = Maintopic.find(params[:maintopic_id])
+    #@subtopic = @maintopic.subtopics.new(params[:subtopic])
     @subtopic = Subtopic.find(params[:id])
 
     respond_to do |format|
@@ -74,4 +88,16 @@ class SubtopicsController < ApplicationController
 #      format.json { head :no_content }
 #    end
 #  end
+   private
+      def load_forum
+         if (params[:id])
+            @subtopic = Subtopic.find(params[:id])
+            @maintopic = @subtopic.maintopic #rescue redirect_to(maintopics_path)
+         elsif (params[:forum_id])
+            @maintopic = Maintopic.find(params[:maintopic_id])
+         else
+            #redirect_to(maintopics_path)
+            #echo "I am here."
+         end
+      end
 end

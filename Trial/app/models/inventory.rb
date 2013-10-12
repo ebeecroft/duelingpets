90989e56e249a386c01726b6c32439d0ea3829ad
Eric <eric@ubuntu.ubuntu-domain>
2013-10-12 12:14:21 -0700

    Modifying equips to work

--- Trial/app/models/inventory.rb
+++ Trial/app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ActiveRecord::Base
    belongs_to :item
    belongs_to :user
-   has_many :equips, :foreign_key => "inventory_id", :dependent => :destroy
+   has_one :equip, :foreign_key => "inventory_id", :dependent => :destroy
 
    validates :user_id, presence: true, :uniqueness => {:scope => :item_id}
    validates :item_id, presence: true

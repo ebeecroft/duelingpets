05a37e66e6666281973c3e49127d09cfa4e58a6d
Eric <ebeecroft@mail.csuchico.edu>
2013-12-30 09:16:58 -0800

    Removed the old single files for items

--- Trial/db/migrate/20131028202806_add_manyuses_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddManyusesToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :manyuses, :boolean
-  end
-end
--- Trial/db/migrate/20131117203812_add_ipicture_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddIpictureToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :ipicture, :string
-  end
-end

0f33bbb9ad4a9df3ffbcf48d00a2de978db31e2d
Eric <ebeecroft@mail.csuchico.edu>
2014-03-01 21:39:25 -0800

    Added punction to regex

--- Trial/app/models/narrative.rb
+++ Trial/app/models/narrative.rb
@@ -2,6 +2,6 @@ class Narrative < ActiveRecord::Base
    belongs_to :subtopic
    belongs_to :user
    attr_accessible :story
-   VALID_NARRATIVE_REGEX = /\A[ A-Za-z0-9:-]+\z/
+   VALID_NARRATIVE_REGEX = /\A[ A-Za-z0-9:!)("',.?-]+\z/
    validates :story, presence: true, format: { with: VALID_NARRATIVE_REGEX}
 end

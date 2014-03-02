527083c647ee961ec3212194371598346c0a0989
Eric <ebeecroft@mail.csuchico.edu>
2014-03-01 20:26:35 -0800

    Fixed bad regex on maintopic

--- Trial/app/models/maintopic.rb
+++ Trial/app/models/maintopic.rb
@@ -4,7 +4,7 @@ class Maintopic < ActiveRecord::Base
    has_many :subtopics, :foreign_key => "maintopic_id", :dependent => :destroy
    attr_accessible :description, :topicname
    VALID_TOPIC_REGEX = /\A[A-Za-z0-9 ]+\z/
-   VALID_DESCRIPTION_REGEX = /\A[A-Za-z0-9:- ]+\z/
+   VALID_DESCRIPTION_REGEX = /\A[ A-Za-z0-9:-]+\z/
    validates :topicname, presence: true, format: { with: VALID_TOPIC_REGEX}
    validates :description, presence: true, format: { with: VALID_DESCRIPTION_REGEX}
 end

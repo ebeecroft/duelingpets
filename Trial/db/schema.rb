7dcf1c416917eb8d9a4bdddd35b89698e358d984
Eric Beecroft <ebeecroft@mail.csuchico.edu>
2015-05-11 05:51:32 +0000

    Added from_user_id to the comments table

--- Trial/db/schema.rb
+++ Trial/db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140424040237) do
+ActiveRecord::Schema.define(:version => 20150511003045) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
     t.string   "author"
     t.text     "message"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.datetime "created_on"
-    t.boolean  "maintenance", :default => false
+    t.boolean  "maintenance",  :default => false
+    t.integer  "from_user_id"
   end
 
   create_table "equips", :force => true do |t|

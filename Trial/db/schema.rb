85dfc16577780d540fa9b2630b0d01955658505c
Eric Beecroft <ebeecroft@mail.csuchico.edu>
2015-05-12 03:24:26 +0000

    Removed the author field from the comments table

--- Trial/db/schema.rb
+++ Trial/db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150511003045) do
+ActiveRecord::Schema.define(:version => 20150512031001) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
-    t.string   "author"
     t.text     "message"
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false

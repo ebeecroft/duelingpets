d9f40d66ae5c555fbee44893f99d70b49ebe2206
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 16:47:38 -0700

    Web 2.0 Maintenance format

--- Trial/app/controllers/maintenancemodes_controller.rb
+++ Trial/app/controllers/maintenancemodes_controller.rb
@@ -1,83 +1,31 @@
 class MaintenancemodesController < ApplicationController
-  # GET /maintenancemodes
-  # GET /maintenancemodes.json
+  include MaintenancemodesHelper
+
   def index
-    @maintenancemodes = Maintenancemode.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @maintenancemodes }
-    end
+     mode "index"
   end
 
-  # GET /maintenancemodes/1
-  # GET /maintenancemodes/1.json
   def show
-    @maintenancemode = Maintenancemode.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @maintenancemode }
-    end
+     mode "show"
   end
 
-  # GET /maintenancemodes/new
-  # GET /maintenancemodes/new.json
   def new
-    @maintenancemode = Maintenancemode.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @maintenancemode }
-    end
+     mode "new"
   end
 
-  # GET /maintenancemodes/1/edit
   def edit
-    @maintenancemode = Maintenancemode.find(params[:id])
+     mode "edit"
   end
 
-  # POST /maintenancemodes
-  # POST /maintenancemodes.json
   def create
-    @maintenancemode = Maintenancemode.new(params[:maintenancemode])
-
-    respond_to do |format|
-      if @maintenancemode.save
-        format.html { redirect_to @maintenancemode, notice: 'Maintenancemode was successfully created.' }
-        format.json { render json: @maintenancemode, status: :created, location: @maintenancemode }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @maintenancemode.errors, status: :unprocessable_entity }
-      end
-    end
+     mode "create"
   end
 
-  # PUT /maintenancemodes/1
-  # PUT /maintenancemodes/1.json
   def update
-    @maintenancemode = Maintenancemode.find(params[:id])
-
-    respond_to do |format|
-      if @maintenancemode.update_attributes(params[:maintenancemode])
-        format.html { redirect_to @maintenancemode, notice: 'Maintenancemode was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @maintenancemode.errors, status: :unprocessable_entity }
-      end
-    end
+     mode "update"
   end
 
-  # DELETE /maintenancemodes/1
-  # DELETE /maintenancemodes/1.json
   def destroy
-    @maintenancemode = Maintenancemode.find(params[:id])
-    @maintenancemode.destroy
-
-    respond_to do |format|
-      format.html { redirect_to maintenancemodes_url }
-      format.json { head :no_content }
-    end
+     mode "destroy"
   end
 end
--- Trial/app/helpers/maintenancemodes_helper.rb
+++ Trial/app/helpers/maintenancemodes_helper.rb
@@ -1,2 +1,57 @@
 module MaintenancemodesHelper
+
+   def mode(type)
+      logged_in = current_user
+      if(logged_in)
+         if(logged_in.admin)
+            switch(type)
+         else
+            redirect_to root_path
+         end
+      else
+         redirect_to root_path
+      end
+   end
+
+   private
+      def switch(type)
+         if(type == "index") #Admin only
+            @maintenancemodes = Maintenancemode.all
+         elsif(type == "show") #Admin only
+            #Not needed since we will only be using index
+         elsif(type == "new") #Admin only
+            @maintenancemode = Maintenancemode.new
+         elsif(type == "create") #Admin only
+            newMaintenancemode = Maintenancemode.new(params[:maintenancemode])
+            currentTime = Time.now
+            newMaintenancemode.created_on = currentTime
+            @maintenancemode = newMaintenancemode
+            if(@maintenancemode.save)
+               redirect_to maintenancemodes_url, notice: 'Maintenancemode was successfully created.'
+            else
+               render "new"
+            end
+         elsif(type == "edit") #Admin only
+            maintenancemodeFound = Maintenancemode.find_by_id(params[:id])
+            if(maintenancemodeFound)
+               @maintenancemode = maintenancemodeFound
+            else
+               render "public/404"
+            end
+         elsif(type == "update") #Admin only
+            maintenancemodeFound = Maintenancemode.find_by_id(params[:id])
+            if(maintenancemodeFound)
+               @maintenancemode = maintenancemodeFound
+               if(@maintenancemode.update_attributes(params[:maintenancemode]))
+                  redirect_to maintenancemodes_url, notice: 'Maintenancemode was successfully updated.'
+               else
+                  render "edit"
+               end
+            else
+               render "public/404"
+            end
+         elsif(type == "destroy") #Admin only
+            #Destroy should not be used
+         end
+      end
 end

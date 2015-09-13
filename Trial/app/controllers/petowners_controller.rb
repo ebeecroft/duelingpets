158917854d2545220e1127307e4b7f99bb144036
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 17:42:26 -0700

    Web 2.0 Petowner format

--- Trial/app/controllers/petowners_controller.rb
+++ Trial/app/controllers/petowners_controller.rb
@@ -1,155 +1,34 @@
 class PetownersController < ApplicationController
-  # GET /petowners
-  # GET /petowners.json
-   def create_adoption
-      params[:pet_id]
-      new_petowner= Petowner.new
-      new_petowner.pet_id = params[:pet_id]
-      new_petowner.user_id = current_user.id
-      new_petowner.save
-
-
-      redirect_to root_url
-   end
-
-  def peterror
-  end
-
-  def list
-     if current_user && current_user.admin?
-        @petowners = Petowner.all
-     else
-        render "public/404"
-     end
-  end
+  include PetownersHelper
 
   def index
-#    @subtopics = @maintopic.subtopics.all
-    @user = User.find_by_vname(params[:user_id])
-    @petowners = @user.petowners.all
-#    @petowners = Petowner.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @petowners }
-    end
+     mode "index"
   end
 
-  # GET /petowners/1
-  # GET /petowners/1.json
   def show
-    @user = User.find_by_vname(params[:user_id])
-    @petowner = Petowner.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @petowner }
-    end
+     mode "show"
   end
 
-  # GET /petowners/new
-  # GET /petowners/new.json
   def new
-    @user = User.find_by_vname(params[:user_id])
-    @pet = Pet.find_by_id(params[:pet_id])  
-    @petowner = @user.petowners.build
-    if @pet.nil?
-       redirect_to petowners_peterror_path
-       return
-    end
-    @petowner.pet_id = @pet.id
-    @petowner.level = @pet.level
-    @petowner.adopted_on = Time.now
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @petowner }
-    end
+     mode "new"
   end
 
-  # GET /petowners/1/edit
   def edit
-    @user = User.find_by_vname(params[:user_id])
-    @petowner = Petowner.find(params[:id])
+     mode "edit"
   end
 
-  # POST /petowners
-  # POST /petowners.json
   def create
-     puts '***********************************************************************'
-     puts params[:pick_pet]
-
-     #Discovers information about the current_user
-     @user = User.find_by_vname(current_user.vname)
-     @pouch = Pouch.find_by_id(@user.id)
-     @petowner = @user.petowners.new(params[:petowner])
-     @pet = Pet.find(@petowner.pet_id)
-     free_pet = false
-
-     #Checks to see if the user doesn't own a single pet
-     if current_user.petowners.empty?
-        #Only allow starter pets to be selected
-        if !@petowner.pet.starter?
-           redirect_to pets_path
-           return
-        end
-        free_pet = true
-     end
-
-     #Decrement the cost of the owned pet from the user's pouch if not the first pet
-     if free_pet == false
-        if @petowner.pet.cost > @pouch.amount
-           redirect_to pets_url
-           return
-        else
-           @pouch.amount -= @petowner.pet.cost #This should be setting change based on user's money - items price
-        end
-     end
-
-     #Sets the stats of the owned pet based on the pet's stats
-     @petowner.level = @pet.level
-     @petowner.hp = @pet.hp
-     @petowner.atk = @pet.atk
-     @petowner.def = @pet.def
-     @petowner.spd = @pet.spd
-     @petowner.hp_max = @pet.hp
-     @petowner.adopted_on = Date.today
-
-     #Saves the new owned pet
-     if @petowner.save
-        @pouch.save
-#        format.html { redirect_to @petowner, notice: 'Petowner was successfully created.' }
-#        format.json { render json: @petowner, status: :created, location: @petowner }
-#maintopic_subtopics_url
-        redirect_to user_petowners_path(@user) #current_user_petowners_url
-     else
-        render "new"
-     end
+     mode "create"
   end
 
-  # PUT /petowners/1
-  # PUT /petowners/1.json
   def update
-#         @maintopic = Maintopic.find(params[:maintopic_id])
-#         @subtopic = Subtopic.find(params[:id])
-    @user = User.find_by_vname(params[:user_id])
-    @petowner = Petowner.find(params[:id])
-
-      if @petowner.update_attributes(params[:petowner])
-        redirect_to user_petowners_path(@user), notice: 'Petowner was successfully updated.' 
-      else
-        render action: "edit" 
-      end
+     mode "update"
   end
 
-  # DELETE /petowners/1
-  # DELETE /petowners/1.json
   def destroy
-    @user = User.find_by_vname(params[:user_id])
-    @petowner = Petowner.find(params[:id])
-    @petowner.destroy
+     mode "destroy"
+  end
 
-    respond_to do |format|
-      format.html { redirect_to user_petowners_url }
-      format.json { head :no_content }
-    end
+  def maintenance
   end
 end
--- Trial/app/helpers/petowners_helper.rb
+++ Trial/app/helpers/petowners_helper.rb
@@ -1,2 +1,262 @@
 module PetownersHelper
+
+   def mode(type)
+      #Check if Maintenance is turned_on
+      allmode = Maintenancemode.find_by_id(1)
+      petownermode = Maintenancemode.find_by_id(4)
+      mode_turned_on = (allmode.maintenance_on || petownermode.maintenance_on)
+      #Determine if any maintenance is on
+      if(mode_turned_on)
+         #Determine if we are a regular user
+         regularUser = (!current_user || !current_user.admin?)
+         if(regularUser)
+            #Determine which maintenance mode is on
+            if(allmode.maintenance_on)
+               redirect_to maintenance_path
+            else
+               redirect_to petowners_maintenance_path
+            end
+         else
+            switch type
+         end
+      else
+         switch type
+      end
+   end
+
+   def getOptional
+      @optional
+   end
+
+   private
+      def savePetowner(purchaseCode)
+         if(@petowner.save)
+            if(purchaseCode == 2)
+               @pouch.save
+            end
+            redirect_to user_petowners_path(@user), notice: 'Petowner was successfully created.'
+         else
+            render "new"
+         end
+      end
+
+      def storeStats(petowner, pet)
+         #Initially sets up pet stats
+         petowner.level = pet.level
+         petowner.hp = pet.hp
+         petowner.atk = pet.atk
+         petowner.def = pet.def
+         petowner.spd = pet.spd
+         petowner.hp_max = pet.hp
+         currentTime = Time.now
+         petowner.adopted_on = currentTime
+         @petowner = petowner
+      end
+
+      def purchase(petCount, petowner, pouch)
+         #Determine the amount of pets the user owns
+         purchaseCode = 0
+         if(petCount > 0)
+            #Determine if the user can afford to purchase the pet
+            moneyLeft = pouch.amount - petowner.pet.cost
+            if(moneyLeft < 0)
+               purchaseCode = 1
+            else
+               purchaseCode = 2
+               pouch.amount = moneyLeft
+               @pouch = pouch
+            end
+         end
+         return purchaseCode
+      end
+
+      def setOptional(optional)
+         @optional = optional
+      end
+
+      def switch(type)
+         if(type == "index") #Guest and Admin
+            optional = params[:user_id]
+            setOptional optional
+            if(getOptional)
+               userFound = User.find_by_vname(optional)
+               if(userFound)
+                  userPets = userFound.petowners.all
+                  @petowners = Kaminari.paginate_array(userPets).page(params[:page]).per(10)
+                  @user = userFound
+               else
+                  render "public/404"
+               end
+            else
+               logged_in = current_user
+               if(logged_in)
+                  if(logged_in.admin)
+                     allPetowners = Petowner.order("id").page(params[:page]).per(10)
+                     @petowners = allPetowners
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  redirect_to root_path
+               end
+            end
+         elsif(type == "show") #Guest
+            userFound = User.find_by_vname(params[:user_id])
+            if(userFound)
+               petownerFound = Petowner.find_by_id(params[:id])
+               if(petownerFound)
+                  userMatch = (userFound.id == petownerFound.user_id)
+                  if(userMatch)
+                     @petowner = petownerFound
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               rendirect_to root_path
+            end
+         elsif(type == "new") #Login only
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:user_id])
+               if(userFound)
+                  userMatch = (logged_in.id == userFound.id)
+                  if(userMatch)
+                     newPetowner = logged_in.petowners.new
+                     petFound = Pet.find_by_id(params[:pet_id])
+                     if(petFound)
+                        newPetowner.pet_id = petFound.id
+                        @petowner = newPetowner
+                        @user = userFound
+                     else
+                        redirect_to pets_path
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "create") #Login only
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:user_id])
+               if(userFound)
+                  userMatch = (logged_in.id == userFound.id)
+                  if(userMatch)
+                     #Initialize the local variables
+                     newPetowner = logged_in.petowners.new(params[:petowner])
+                     pouchFound = Pouch.find_by_id(newPetowner.user_id)
+                     #Purchases the pet
+                     purchaseCode = purchase(logged_in.petowners.count, newPetowner, pouchFound)
+                     #Stores the petowner's stats
+                     storeStats(newPetowner, newPetowner.pet)
+                     #Checks the return code to see if the pet can be bought
+                     if(purchaseCode == 1)
+                        redirect_to pets_path
+                     else
+                        @user = userFound
+                        if(purchaseCode == 0)
+                           #Allows the purchase only if the user has selected a starter pet
+                           starterPet = newPetowner.pet.starter
+                           if(starterPet)
+                              savePetowner(purchaseCode)
+                           else
+                              redirect_to pets_path
+                           end
+                        else
+                           savePetowner(purchaseCode)
+                        end
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "edit") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(logged_in.vname)
+               if(userFound)
+                  petownerFound = Petowner.find_by_id(params[:id])
+                  if(petownerFound)
+                     userMatch = ((userFound.id == petownerFound.user_id) || logged_in.admin)
+                     if(userMatch)
+                        @user = userFound
+                        @petowner = petownerFound
+                     else
+                        redirect_to root_path
+                     end
+                  else
+                     render "public/404"
+                  end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "update") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(logged_in.vname)
+               if(userFound)
+                  petownerFound = Petowner.find_by_id(params[:id])
+                  if(petownerFound)
+                     userMatch = ((userFound.id == petownerFound.user_id) || logged_in.admin)
+                     if(userMatch)
+                        @petowner = petownerFound
+                        if(@petowner.update_attributes(params[:petowner]))
+                           @user = userFound
+                           redirect_to user_petowner_path(@user, @petowner), notice: 'Petowner was successfully updated.'
+                        else
+                           render "edit"
+                        end
+                     else
+                        redirect_to root_path
+                     end
+                  else
+                     render "public/404"
+                  end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "destroy") #Admin
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  petownerFound = Petowner.find_by_id(params[:id])
+                  if(petownerFound)
+                     userFound = User.find_by_vname(petownerFound.user_id)
+                     if(userFound)
+                        @petowner = petownerFound
+                        @user = userFound
+                        @petowner.destroy
+                        redirect_to user_petowners_path(@user)
+                     else
+                        redirect_to root_path
+                     end
+                  else
+                     render "public/404"
+                  end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         end
+      end
 end

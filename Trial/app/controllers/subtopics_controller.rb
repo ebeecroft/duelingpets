b5787a6717fe818af8b51be5d5ea04b184d1b3d0
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 18:01:28 -0700

    Web 2.0 Subtopic format

--- Trial/app/controllers/subtopics_controller.rb
+++ Trial/app/controllers/subtopics_controller.rb
@@ -1,108 +1,34 @@
 class SubtopicsController < ApplicationController
-  # GET /subtopics
-  # GET /subtopics.json
-  #before_filter :load_forum
-
-  before_filter :load_topic, :only => [:edit, :update, :show, :destroy]
-  before_filter :load_maintopic, :only =>[:create, :index, :new]
+  include SubtopicsHelper
 
   def index
-     if current_user && current_user.admin?
-        @subtopics = @maintopic.subtopics.all
-     else
-        render "public/404"
-     end
+     mode "index"
   end
 
-  # GET /subtopics/1
-  # GET /subtopics/1.json
   def show
-    #@subtopic.narratives.page(params[:page])
-    @narrative = Kaminari.paginate_array(@subtopic.narratives).page(params[:page]).per(25)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @subtopic }
-    end
+     mode "show"
   end
 
-  # GET /subtopics/new
-  # GET /subtopics/new.json
   def new
-     if current_user
-        @user = current_user.id
-        @subtopic = @maintopic.subtopics.build
-        @subtopic.user_id = @user
-     else
-        redirect_to root_url
-     end
+     mode "new"
   end
 
-  # GET /subtopics/1/edit
   def edit
+     mode "edit"
   end
 
-  # POST /subtopics
-  # POST /subtopics.json
   def create
-     if current_user
-        @user = current_user.id
-        @subtopic = @maintopic.subtopics.new(params[:subtopic])
-        @subtopic.user_id = @user
-        if !(@subtopic.maintopic_id == @maintopic.id) #Prevents a subtopic from being assigned data to a maintopic that doesn't match
-           redirect_to @maintopic
-           return
-        end
-
-        @subtopic.created_on = Time.now
-        @subtopic.save
-        #if @subtopic.save
-        #   redirect_to @maintopic.subtopic
-        #else
-        #   render "new";
-        #end
-        redirect_to maintopic_subtopic_path(@maintopic, @subtopic)
-     else
-        redirect_to root_url
-     end
+     mode "create"
   end
 
-  # PUT /subtopics/1
-  # PUT /subtopics/1.json
   def update
-    respond_to do |format|
-      if @subtopic.update_attributes(params[:subtopic])
-        format.html { redirect_to maintopic_subtopic_path(@subtopic.maintopic_id, @subtopic.id), notice: 'Subtopic was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @subtopic.errors, status: :unprocessable_entity }
-      end
-    end
+     mode "update"
   end
-#raise
-  # DELETE /subtopics/1
-  # DELETE /subtopics/1.json
-  def destroy
-    @subtopic.destroy
-    
-    respond_to do |format|
-      format.html { redirect_to tcontainer_maintopic_path(@maintopic.tcontainer_id, @maintopic.id) }
-      format.json { head :no_content }
-    end
-  end
-   private
-      def load_topic
-        @subtopic = Subtopic.find(params[:id])
-        @maintopic = Maintopic.find(@subtopic.maintopic_id)
-        @content = Maintopic.find(params[:maintopic_id])
-        if @content.id != @maintopic.id
-#           raise "I been tampered with and should redirect to the root page"
-           redirect_to root_url
-        end
-      end
 
-      def load_maintopic
-         @maintopic = Maintopic.find(params[:maintopic_id])
-      end
+  def destroy
+     mode "destroy"
+  end
+
+  def maintenance
+  end
 end

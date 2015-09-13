b4ea15c0d0385218105d8459017dd2b2bfa31322
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 17:47:13 -0700

    Web 2.0 Pet format

--- Trial/app/controllers/pets_controller.rb
+++ Trial/app/controllers/pets_controller.rb
@@ -1,211 +1,54 @@
 class PetsController < ApplicationController
-   # GET /pets
-   # GET /pets.json
+   include PetsHelper
 
-   def monsters
-      #Finds the current user
-      if current_user
-         @user = current_user
-         @mypets=[]      
-         @selectpet = 1 #Makes the fight work
-         @pet = Pet.find_by_id(params[:pet_id])
-         #This is my way of doing things in a C++ fashion, I don't get all ruby things
-         @user.petowners.each do |pet|
-           #@selectpet2 = (params[:petowner][:id])
-           #@selected_pet = pet.find(params[:selected])
-       #   if pet.hp != 0
-       #      @pets_kept << pet #Dont recall seeing the << before in ruby but for C++ statement used for cout statements
-             #if pet.select
-             #   @selected_pet = pet.select
-             #end
-         end
-         @mypets = current_user.petowners.select{|pet| pet.in_battle != true && pet.hp !=0}
-      end
-
-      @pets = Pet.all
-      @count = 0
-      @count3 = 0
-      @pets.each do |pet|
-         if pet.reviewed? && pet.monster?
-            @count+=1
-         end
-      end
+   def index
+      mode "index"
    end
 
-   def reviews
-      if current_user && current_user.admin?
-         @pets = Pet.all
-      else
-         redirect_to root_url
-      end
+   def show 
+      mode "show"
    end
 
-   def deny
-      if current_user && current_user.admin?
-         @pet = Pet.find_by_id(params[:pet_id])
-         #send_email_to user that pet has been denied and be able to resend it
-         redirect_to pets_reviews_url
-      else
-         redirect_to root_url
-      end
+   def new
+      mode "new"
    end
 
-   def approve
-      if current_user && current_user.admin?
-         @pet = Pet.find_by_id(params[:pet_id])
-         @pet.reviewed = true
-         @pet.update_attributes(params[:pet])
-         redirect_to pets_reviews_url
-      else
-         redirect_to root_url
-      end
+   def edit
+      mode "edit"
+   end
+
+   def create
+      mode "create"
+   end
+
+   def update
+      mode "update"
+   end
+
+   def destroy
+      mode "destroy"
    end
 
    def list
-      if current_user && current_user.admin?
-         @pets = Pet.all
-      else
-         render "public/404"
-      end
+      mode "list"
    end
 
-   def index
-      @count = 0
-      @count2 = 0
-      @pets = Pet.all
-      @pets.each do |pet|
-         if pet.reviewed?
-            @count+=1
-         end
-      end
-
-      respond_to do |format|
-         format.html # index.html.erb
-         format.json { render json: @pets }
-      end
+   def review
+      mode "review"
    end
 
-   # GET /pets/1
-   # GET /pets/1.json
-   def show 
-      @pet = Pet.find_by_species_name(params[:id])
-      #@pet_owner.adopted_on = Date.today
-      if !@pet.reviewed?
-         redirect_to root_path
-      end
+   def approve
+      mode "approve"
    end
 
-   # GET /pets/new
-   # GET /pets/new.json
-   def new
-      @pet = Pet.new
-
-      respond_to do |format|
-         format.html # new.html.erb
-         format.json { render json: @pet }
-      end
+   def deny
+      mode "deny"
    end
 
-   # GET /pets/1/edit
-   def edit
-      @pet = Pet.find_by_species_name(params[:id])
+   def monsters
+      mode "monsters"
    end
 
-   # POST /pets
-   # POST /pets.json
-   def create
-      #@comment.author = current_user.vname
-      @pet = Pet.new(params[:pet])
-      #flash[:success]
-      @invalid = 0
-
-      #raise "I work after the invalid variable"
-
-         if @pet.hp < 10
-            @invalid = 1
-            flash[:notice] = "HP is lower then the minimum of 10 please correct" #{@contact.name}.
-            render "new"
-            return
-         end
-
-         if @pet.atk < 5 && @invalid!=1
-            @invalid = 1
-            flash[:notice] = "ATK is lower then the minimum of 5 please correct"
-            render "new"
-            return
-         end
-
-         if @pet.def < 5 && @invalid!=1
-            @invalid = 1
-            flash[:notice] = "DEF is lower then the minimum of 5 please correct"
-            render "new"
-            return
-         end
-
-         if @pet.spd < 5 && @invalid!=1
-            @invalid = 1
-            flash[:notice] = "SPD is lower then the minimum of 5 please correct"
-            render "new"
-            return
-         end
-
-      #raise "I work for succesful pet attributes"
-
-      #    @pet.cost = (1+(@pet.hp/10) + (@pet.atk/5) + (@pet.def/5) + (@pet.spd/5))*10
-      health = @pet.hp
-      attack = @pet.atk
-      defense = @pet.def
-      speed = @pet.spd
-
-      #Sends the Ruby data into the C++ calc program and retrieves the result
-      results = `calc/calc #{health} #{attack} #{defense} #{speed}`
-      #raise "I work after sending my attributes to the c++ calc program"
-
-      string_array = results.split(",")
-      r_cost, r_level = string_array.map { |str| str.to_i }
-
-      @pet.cost = r_cost
-      @pet.level = r_level
-      @pet.created_on = Time.now
-      @pet.created_by = current_user.vname
-      #raise "I work after setting the pet cost, level, date of pet created and the creator of the pet"
-
-      respond_to do |format|
-         if @pet.save
-            format.html { redirect_to pets_url, notice: 'Pet was successfully created.' }
-            format.json { render json: @pet, status: :created, location: @pet }
-         else
-            format.html { render action: "new" }
-            format.json { render json: @pet.errors, status: :unprocessable_entity }
-         end
-      end
-   end
-
-   # PUT /pets/1
-   # PUT /pets/1.json
-   def update
-      @pet = Pet.find_by_species_name(params[:id])
-
-      respond_to do |format|
-         if @pet.update_attributes(params[:pet])
-            format.html { redirect_to @pet, notice: 'Pet was successfully updated.' }
-            format.json { head :no_content }
-         else
-            format.html { render action: "edit" }
-            format.json { render json: @pet.errors, status: :unprocessable_entity }
-         end
-      end
-   end
-
-   # DELETE /pets/1
-   # DELETE /pets/1.json
-   def destroy
-      @pet = Pet.find_by_species_name(params[:id])
-      @pet.destroy
-
-      respond_to do |format|
-         format.html { redirect_to pets_url }
-         format.json { head :no_content }
-      end
+   def maintenance
    end
 end
--- Trial/app/helpers/pets_helper.rb
+++ Trial/app/helpers/pets_helper.rb
@@ -1,2 +1,304 @@
 module PetsHelper
+
+   def mode(type)
+      #Check if Maintenance is turned_on
+      allmode = Maintenancemode.find_by_id(1)
+      petmode = Maintenancemode.find_by_id(3)
+      mode_turned_on = (allmode.maintenance_on || petmode.maintenance_on)
+      #Determine if any maintenance is on
+      if(mode_turned_on)
+         #Determine if we are a regular user
+         regularUser = (!current_user || !current_user.admin?)
+         if(regularUser)
+            #Determine which maintenance mode is on
+            if(allmode.maintenance_on)
+               redirect_to maintenance_path
+            else
+               redirect_to pets_maintenance_path
+            end
+         else
+            switch type
+         end
+      else
+         switch type
+      end
+   end
+
+   def getPetType
+      @petType
+   end
+
+   private
+      def petType(type)
+         allPets = Pet.all
+         reviewedPets = allPets.select{|pet| pet.reviewed}
+         if(type == "pet")
+            pets = reviewedPets.select{|pet| !pet.monster}
+            petCount = pets.count
+            if(current_user && !current_user.admin)
+               ownedPetCount = current_user.petowners.count
+               if(ownedPetCount > 0)
+                  @pets = Kaminari.paginate_array(pets).page(params[:page]).per(9)
+               else
+                  starterPets = pets.select{|pet| pet.starter}
+                  @pets = Kaminari.paginate_array(starterPets).page(params[:page]).per(9)
+               end
+            else
+               @pets = Kaminari.paginate_array(pets).page(params[:page]).per(9)
+            end
+            @count = petCount
+         elsif(type == "monster")
+            monsters = reviewedPets.select{|pet| pet.monster}
+            monsterCount = monsters.count
+            @pets = Kaminari.paginate_array(monsters).page(params[:page]).per(9)
+            @count = monsterCount
+         end
+         @petType = type
+      end
+
+      def calculator(health, attack, defense, speed)
+         results = `calc/calc #{health} #{attack} #{defense} #{speed}`
+         string_array = results.split(",")
+         return string_array
+      end
+
+      def validator(health, attack, defense, speed)
+         #Minimum pet values
+         minHealth = 10
+         minAttack = 5
+         minDefense = 5
+         minSpeed = 5
+
+         #Determines if the pet is valid
+         errorFlag = false
+         validPet = ((health >= minHealth && attack >= minAttack) && (defense >= minDefense && speed >= minSpeed))
+         if(validPet)
+            #Does nothing since this pet is valid
+         else
+            if(health < minHealth)
+               flash[:notice] = "HP is below minimum value of 10"
+            end
+
+            if(attack < minAttack)
+               flash[:notice] = "ATK is below minimum value of 5"
+            end
+
+            if(defense < minDefense)
+               flash[:notice] = "DEF is below minimum value of 5"
+            end
+
+            if(speed < minSpeed)
+               flash[:notice] = "SPD is below minimum value of 5"
+            end
+            errorFlag = true
+         end
+         return errorFlag
+      end
+
+      def switch(type)
+         if(type == "index") #Guest
+            petType("pet")
+#            @count2 = 0
+         elsif(type == "show") #Guest
+            #We want to show pets regardless of review status
+            petFound = Pet.find_by_species_name(params[:id])
+            if(petFound)
+               #Pets that haven't been reviewed should only be visable to the user who created them
+               if(petFound.reviewed)
+                  @pet = petFound
+               else
+                  logged_in = current_user
+                  if(logged_in)
+                     userMatch = (logged_in.vname == petFound.created_by) #Very bad design decision
+                     if(userMatch)
+                        @pet = petFound
+                     else
+                        redirect_to root_path
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               end
+            else
+               render "public/404"
+            end
+         elsif(type == "new") #Login only
+            logged_in = current_user
+            if(logged_in)
+               newPet = Pet.new
+               @pet = newPet
+            else
+               redirect_to root_path
+            end
+         elsif(type == "create") #Login only
+            logged_in = current_user
+            if(logged_in)
+               newPet = Pet.new(params[:pet])
+               #Need to check if the pet is valid first before proceeding
+               errorFlag = validator(newPet.hp, newPet.atk, newPet.def, newPet.spd)
+               if(errorFlag)
+                  @pet = newPet
+                  render "new"
+               else
+                  #Need to calculate the pet's cost and level
+                  string_array = calculator(newPet.hp, newPet.atk, newPet.def, newPet.spd)
+                  petCost, petLevel = string_array.map { |str| str.to_i}
+                  newPet.cost = petCost
+                  newPet.level = petLevel
+                  currentTime = Time.now
+                  newPet.created_on = currentTime
+                  newPet.created_by = logged_in.vname
+                  #Add the pet to the database
+                  @pet = newPet
+                  if(@pet.save)
+                     flash[:success] = 'Pet was successfully created.'
+                     redirect_to pets_url
+                  else
+                     render "new"
+                  end
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "edit") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               petFound = Pet.find_by_species_name(params[:id])
+               if(petFound)
+                  userMatch = ((logged_in.vname == petFound.created_by) || logged_in.admin)
+                  if(userMatch)
+                     @pet = petFound
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "update") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               petFound = Pet.find_by_species_name(params[:id])
+               if(petFound)
+                  userMatch = ((logged_in.vname == petFound.created_by) || logged_in.admin)
+                  if(userMatch)
+                     @pet = petFound
+                     if(@pet.update_attributes(params[:pet]))
+                        flash[:success] = 'Pet was successfully updated.'
+                        redirect_to @pet
+                     else
+                        render "edit"
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "destroy") #Admin
+            logged_in = current_user
+            if(logged_in)
+               petFound = Pet.find_by_species_name(params[:id])
+               if(petFound)
+                  if(logged_in.admin)
+                     @pet = petFound
+                     @pet.destroy
+                     flash[:success] = 'Pet was succesfully removed.'
+                     redirect_to pets_url
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "list") #Admin
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  allPets = Pet.order("id").page(params[:page]).per(10)
+                  @pets = allPets
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "review") #Admin
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  allPets = Pet.all
+                  petsToReview = allPets.select{|pet| !pet.reviewed}
+                  @pets = Kaminari.paginate_array(petsToReview).page(params[:page]).per(2)
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "approve") #Admin
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  petFound = Pet.find_by_id(params[:pet_id])
+                  if(petFound)
+                     petFound.reviewed = true
+                     @pet = petFound
+                     @pet.save
+                     redirect_to pets_review_path
+                  else
+                     render "public/404"
+                  end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "deny") #Admin
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  petFound = Pet.find_by_id(params[:pet_id])
+                  if(petFound)
+                     #Retrieve the user who owns this pet first
+                     #userEmail = petFound.user.email
+                     #Send mail to user with link to edit the pet they sent
+                     @pet = petFound
+                     redirect_to pets_review_path
+                  else
+                     render "public/404"
+                  end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "monsters") #Login only
+            logged_in = current_user
+            if(logged_in)
+               petType("monster")
+               #Petowner List
+               healthyPets = logged_in.petowners.select{|pet| pet.hp != 0}
+               availablePets = healthyPets.select{|pet| pet.in_battle != true}
+               @mypets = availablePets
+               @selectpet = logged_in.petowners.minimum(:id)
+               #@petselected = availablePets.minimum()
+               #Might need @pet here for last parameter
+            else
+               redirect_to root_path
+            end
+         else
+            raise "Invalid selection was chosen."
+         end
+      end
 end

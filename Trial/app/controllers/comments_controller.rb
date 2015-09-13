06132e476ac7b86134639fc3f2650fb67166bf74
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 17:07:28 -0700

    Web 2.0 Comment format

--- Trial/app/controllers/comments_controller.rb
+++ Trial/app/controllers/comments_controller.rb
@@ -1,89 +1,18 @@
 class CommentsController < ApplicationController
-  # GET /comments
-  # GET /comments.json
-  #include SessionsHelper
+  include CommentsHelper
+
   def index
-     if current_user && current_user.admin?
-        @user = User.find_by_vname(params[:user_id])
-        @comments = @user.comments.all
-     else
-        render "public/404"
-     end
+     mode "index"
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  # GET /comments/new
-  # GET /comments/new.json
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  # GET /comments/1/edit
-  def edit
-#  @maintopic = Maintopic.find(params[:maintopic_id])
-#  @subtopic = Subtopic.find(params[:id])
-    @user = User.find_by_vname(params[:user_id])
-    @comment = Comment.find(params[:id])
-  end
-
-  # POST /comments
-  # POST /comments.json
   def create
-     @user = User.find_by_vname(params[:user_id])
-     @comment = @user.comments.new(params[:comment])
-     @comment.created_on = Time.now
-     @comment.from_user_id = current_user.id
-
-     if @comment.save
-        #@comment.author.inspect
-        #raise "I am saving the comment"
-     else
-        raise "I couldn't save"
-     end
-     redirect_to @user
+     mode "create"
   end
 
-  # PUT /comments/1
-  # PUT /comments/1.json
-  def update
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
-    @user = User.find_by_vname(params[:user_id])
-    @comment = Comment.find(params[:id])
-    @comment.destroy
+     mode "destroy"
+  end
 
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.json { head :no_content }
-    end
+  def maintenance
   end
 end
--- Trial/app/helpers/comments_helper.rb
+++ Trial/app/helpers/comments_helper.rb
@@ -1,2 +1,63 @@
 module CommentsHelper
+
+   def mode(type)
+      switch type
+   end
+
+   private
+      def switch(type)
+         if(type == "index") #Admin only
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  allComments = Comment.order("id").page(params[:page]).per(10)
+                  @comments = allComments
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "create") #logged in only
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:user_id])
+               if(userFound)
+                  newComment = userFound.comments.new(params[:comment])
+                  newComment.from_user_id = logged_in.id
+                  currentTime = Time.now
+                  newComment.created_on = currentTime
+                  @comment = newComment
+                  @comment.save
+                  @user = userFound
+                  redirect_to @user
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "destroy") #logged in only
+            logged_in = current_user
+            if(logged_in)
+               commentFound = Comment.find(params[:id])
+               if(commentFound)
+                  userFound = User.find_by_vname(params[:user_id])
+                  userMatch = ((logged_in.id == commentFound.from_user_id) || logged_in.admin)
+                  if(userMatch)
+                     @user = userFound
+                     @comment = commentFound
+                     @comment.destroy
+                     redirect_to userFound, notice: 'Comment was successfully removed.'
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         end
+      end
 end

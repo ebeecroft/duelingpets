47c0385f7d065a67ab53cee50fcb51109ff5e798
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 18:14:34 -0700

    Web 2.0 User format

--- Trial/app/controllers/users_controller.rb
+++ Trial/app/controllers/users_controller.rb
@@ -1,117 +1,34 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, :only =>[:edit, :update, :destroy]
-  before_filter :correct_user, :only=>[:edit, :update]
-  before_filter :admin, :only=>[:destory]
-  # GET /users
-  # GET /users.json
+  include UsersHelper
+
   def index
-    if current_user && current_user.admin
-       @users = User.all
-
-       respond_to do |format|
-          format.html # index.html.erb
-          format.json { render json: @users }
-       end
-    else
-       render "public/404"
-    end
+     mode "index"
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-    @user = User.find_by_vname(params[:id])
-    if @user
-       @pouch = Pouch.find_by_id(@user.id)
-       @count = 0
-       @user.comments.each do |comment|
-          @count+=1
-       end
-    else
-       render "public/404"
-    end
-    #raise "I am here"
-
-#    if @user.maintenance?
-#       raise "I am in maintenance mode"
-#    end
+     mode "show"
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
+     mode "new"
   end
 
-  # GET /users/1/edit
   def edit
+     mode "edit"
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(params[:user])
-    #start_time = current_time.strftime("%m/%d/%Y")
-    #@user.joined_on = (Date.today.to_s.to_date)
-    @user.joined_on = Time.now
-      if @user.save
-         @pouch = Pouch.new(params[:pouch])
-         #@pouch = @user.pouches.build
-         @pouch.user_id = @user.id
-         @pouch.save
-         sign_in @user
-         flash[:success] = "Welcome to the Trial app"
-         redirect_to @user
-      else
-         render "new"
-      end
+     mode "create"
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
   def update
-    @user = User.find_by_vname(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        sign_in @user
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+     mode "update"
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user = User.find_by_vname(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+     mode "destroy"
   end
 
-  private
-     def signed_in_user
-        redirect_to signin_path unless signed_in?
-     end
-
-     def correct_user
-        @user = User.find_by_vname(params[:id])
-        redirect_to root_path unless (current_user?(@user) || current_user.admin?)
-     end
-
-     def admin
-        redirect_to root_path unless (current_user && current_user.admin?)
-     end
+  def maintenance
+  end
 end
--- Trial/app/helpers/users_helper.rb
+++ Trial/app/helpers/users_helper.rb
@@ -1,2 +1,147 @@
 module UsersHelper
+
+   def mode(type)
+      #Check if Maintenance is turned_on
+      allmode = Maintenancemode.find_by_id(1)
+      usermode = Maintenancemode.find_by_id(2)
+      mode_turned_on = (allmode.maintenance_on || usermode.maintenance_on)
+      #Determine if any maintenance is on
+      if(mode_turned_on)
+         #Determine if we are a regular user
+         regularUser = (!current_user || !current_user.admin?)
+         if(regularUser)
+            #Determine which maintenance mode is on
+            if(allmode.maintenance_on)
+               redirect_to maintenance_path
+            else
+               redirect_to users_maintenance_path
+            end
+         else
+            switch type
+         end
+      else
+         switch type
+      end
+   end
+
+   private
+      def switch(type)
+         if(type == "index") #Admin only
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
+                  @users = User.order("id").page(params[:page]).per(10)
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "show") #Guest access
+            userFound = User.find_by_vname(params[:id])
+            if(userFound)
+               @pouch = Pouch.find_by_id(userFound.id)
+               commentCount = 0 #May remove this variable in a later build
+               allComments = Comment.all
+               userComments = allComments.select{|comment| comment.user_id == userFound.id}
+               @comment = Kaminari.paginate_array(userComments).page(params[:page]).per(10)
+               commentCount = userComments.count
+               forumCount = 0
+               userFound.forums.each do |forum|
+                  forumCount += 1
+               end
+               @fcount = forumCount
+               @count = commentCount
+               @user = userFound
+            else
+               render "public/404"
+            end
+         elsif(type == "new") #Guest access
+            @user = User.new
+         elsif(type == "create") #Guest access
+            newMember = User.new(params[:user])
+            currentTime = Time.now
+            newMember.joined_on = currentTime
+            @user = newMember
+            if(@user.save)
+               newPouch = Pouch.new(params[:pouch])
+               newPouch.user_id = newMember.id
+               @pouch = newPouch
+               @pouch.save
+               sign_in newMember
+               flash[:success] = "Welcome to the Duelingpets Website!"
+               redirect_to @user
+            else
+               render "new"
+            end
+         elsif(type == "edit") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:id])
+               if(userFound)
+                  userMatch = ((logged_in.id == userFound.id) || logged_in.admin)
+                  if(userMatch)
+                     @user = userFound
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "update") #Login only and same user
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:id])
+               if(userFound)
+                  userMatch = ((logged_in.id == userFound.id) || logged_in.admin)
+                  if(userMatch)
+                     @user = userFound
+                     if(@user.update_attributes(params[:user]))
+                        if(!logged_in.admin || logged_in == userFound)
+                           sign_in @user
+                        end
+                        flash[:success] = 'User was successfully updated.'
+                        redirect_to @user
+                        #redirect_to @user, notice: 'User was successfully updated.'
+                     else
+                        render "edit"
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         elsif(type == "destroy") #Admin only
+            logged_in = current_user
+            if(logged_in)
+               userFound = User.find_by_vname(params[:id])
+               if(userFound)
+                  if(logged_in.admin)
+                     if(!userFound.admin)
+                        @user = userFound
+                        @user.destroy
+                        flash[:success] = 'User was successfully removed.'
+                        redirect_to users_url
+                        #redirect_to users_url, notice: 'User was successfully removed.'
+                     else
+                        redirect_to root_path
+                     end
+                  else
+                     redirect_to root_path
+                  end
+               else
+                  render "public/404"
+               end
+            else
+               redirect_to root_path
+            end
+         end
+      end
 end

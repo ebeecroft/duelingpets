74a5f00d6f1665a5f4e7819c043b08b7a241e3ac
Eric <ebeecroft@mail.csuchico.edu>
2015-09-12 17:20:51 -0700

    Web 2.0 forum format

--- Trial/app/controllers/forums_controller.rb
+++ Trial/app/controllers/forums_controller.rb
@@ -1,110 +1,38 @@
 class ForumsController < ApplicationController
-  # GET /forums
-  # GET /forums.json
+  include ForumsHelper
 
-  def list
-     if current_user && current_user.admin?
-        @forums = Forum.all
-     else
-        render "public/404"
-     end
+  def index #Guest access
+     mode "index"
   end
 
-  def index
-    @user = User.find_by_vname(params[:user_id])
-    if @user.nil?
-       if params[:user_id]
-          #raise "I am not empty"
-          render "public/404"
-          return
-       end
-       @forums = Forum.all
-    else
-       @forums = @user.forums.all
-    end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @forums }
-    end
+  def show #Guest access
+     mode "show"
   end
 
-  # GET /forums/1
-  # GET /forums/1.json
-  def show
-    @forum = Forum.find_by_name(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @forum }
-    end
+  def new #Logged in only
+     mode "new"
   end
 
-  # GET /forums/new
-  # GET /forums/new.json
-  def new
-    @user = current_user
-#    @user = User.find_by_vname(params[:user_id])
-#    @user = User.find_by_vname(params[:user_id])
-    @forum = @user.forums.build
-#    @forum = Forum.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @forum }
-    end
+  def edit #Logged in only
+     mode "edit"
   end
 
-  # GET /forums/1/edit
-  def edit
-    @user = User.find_by_vname(params[:user_id])
-    @forum = Forum.find_by_name(params[:id])
+  def create #Logged in only
+     mode "create"
   end
 
-  # POST /forums
-  # POST /forums.json
-  def create
-    @user = current_user
-    @forum = @user.forums.new(params[:forum])
-    @forum.created_on = Time.now
-
-    respond_to do |format|
-      if @forum.save
-        format.html { redirect_to user_forum_path(@user, @forum), notice: 'Forum was successfully created.' }
-        format.json { render json: @forum, status: :created, location: @forum }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @forum.errors, status: :unprocessable_entity }
-      end
-    end
+  def update #Logged in only
+     mode "update"
   end
 
-  # PUT /forums/1
-  # PUT /forums/1.json
-  def update
-    @forum = Forum.find_by_name(params[:id])
-
-    respond_to do |format|
-      if @forum.update_attributes(params[:forum])
-        format.html { redirect_to user_forum_path(@forum.user, @forum), notice: 'Forum was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @forum.errors, status: :unprocessable_entity }
-      end
-    end
+  def destroy #Admin only
+     mode "destroy"
   end
 
-  # DELETE /forums/1
-  # DELETE /forums/1.json
-  def destroy
-    @user = User.find_by_vname(params[:user_id])
-    @forum = Forum.find_by_name(params[:id])
-    @forum.destroy
+  def list #Admin only
+     mode "list"
+  end
 
-    respond_to do |format|
-      format.html { redirect_to forums_url }
-      format.json { head :no_content }
-    end
+  def maintenance
   end
 end

5e13395b6ecc15753918fabc6f6c7bf3e522aa31
Eric <ebeecroft@mail.csuchico.edu>
2016-01-23 09:56:05 -0800

    Locking down sessionkeys

--- Trial/app/helpers/sessionkeys_helper.rb
+++ Trial/app/helpers/sessionkeys_helper.rb
@@ -13,44 +13,44 @@ module SessionkeysHelper
    private
       def switch(type)
          if(type == "index") #Admin only
-            #logged_in = current_user
-            #if(logged_in)
-            #   if(logged_in.admin)
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
                   @sessionkeys = Sessionkey.order("id").page(params[:page]).per(10)
-            #   else
-            #      redirect_to root_path
-            #   end
-            #else
-            #   redirect_to root_path
-            #end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
          elsif(type == "new") #Admin only
-            #logged_in = current_user
-            #if(logged_in)
-            #   if(logged_in.admin)
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
                   newKey = Sessionkey.new
                   @sessionkey = newKey
                   #@user = logged_in
-           #    else
-           #       redirect_to root_path
-           #    end
-           # else
-           #    redirect_to root_path
-           # end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
          elsif(type == "create") #Admin only
-            #logged_in = current_user
-            #if(logged_in)
-            #   if(logged_in.admin)
+            logged_in = current_user
+            if(logged_in)
+               if(logged_in.admin)
                   newKey = Sessionkey.new(params[:sessionkey])
                   newKey.remember_token = "NULL"
                   #newKey.user_id = logged_in.id
                   @sessionkey = newKey
                   @sessionkey.save
-            #   else
-            #      redirect_to root_path
-            #   end
-            #else
-            #   redirect_to root_path
-            #end
+               else
+                  redirect_to root_path
+               end
+            else
+               redirect_to root_path
+            end
          end
       end
 end

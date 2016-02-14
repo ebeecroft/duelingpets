ad66f6817a708e5c49ee4474de4601cd530d4bd5
Eric <ebeecroft@mail.csuchico.edu>
2016-02-13 17:46:43 -0800

    Fixed model issue for subfolder

--- Trial/app/models/subfolder.rb
+++ Trial/app/models/subfolder.rb
@@ -1,3 +1,8 @@
 class Subfolder < ActiveRecord::Base
-  attr_accessible :collab_mode, :created_on, :description, :mainfolder_id, :maintenance, :name, :user_id
+   attr_accessible :name, :description, :collab_mode
+   belongs_to :mainfolder
+   belongs_to :user
+   has_many :artworks, :foreign_key => "subfolder_id", :dependent => :destroy
+   VALID_NAME = /\A[A-Za-z][A-Za-z][A-Za-z ]+\z/
+   validates :name, presence: true, format: {with: VALID_NAME}
 end

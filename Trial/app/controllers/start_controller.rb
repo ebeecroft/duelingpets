8c8a76a432a58504bda1472c18f3ff845215882d
Eric <ebeecroft@mail.csuchico.edu>
2016-03-04 20:50:18 -0800

    Refactored the controllers

--- Trial/app/controllers/start_controller.rb
+++ Trial/app/controllers/start_controller.rb
@@ -1,13 +1,19 @@
 class StartController < ApplicationController
 
    def home
-      #Check if Maintenance is turned_on
-      maintenancemode = Maintenancemode.find_by_id(1)
-      if(maintenancemode.maintenance_on)
-         #Determine if we are a regular user
-         regularUser = (!current_user || !current_user.admin?)
-         if(regularUser)
-            redirect_to maintenance_path
+      code = auto_logout
+      if(code == true)
+         sign_out
+         redirect_to root_path
+      else
+         #Check if Maintenance is turned_on
+         maintenancemode = Maintenancemode.find_by_id(1)
+         if(maintenancemode.maintenance_on)
+            #Determine if we are a regular user
+            regularUser = (!current_user || !current_user.admin?)
+            if(regularUser)
+               redirect_to maintenance_path
+            end
          end
       end
    end

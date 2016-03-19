43d269ffddb63700a939c08efc074f504bb99b2e
Eric Beecroft <ebeecroft@mail.csuchico.edu>
2016-03-19 01:06:43 +0000

    Made assets cookieless

--- Trial/config/initializers/session_store.rb
+++ Trial/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Trial::Application.config.session_store :cookie_store, key: '_Trial_session'
+Trial::Application.config.session_store :cookie_store, key: '_Trial_session', domain: 'www.example.com'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

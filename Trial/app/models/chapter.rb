f66f1a4960457cb54aaa49617661c6784b4c3177
Eric <ebeecroft@mail.csuchico.edu>
2016-05-15 00:19:14 -0700

    Repaired broken model

--- Trial/app/models/chapter.rb
+++ Trial/app/models/chapter.rb
@@ -1,7 +1,7 @@
 class Chapter < ActiveRecord::Base
-   attr_accessible :title, :story, :document, :remote_document_url
+   attr_accessible :title, :story #:document, :remote_document_url
    belongs_to :book
    belongs_to :gchapter
    belongs_to :user
-   mount_uploader :document, DocumentUploader
+   #mount_uploader :document, DocumentUploader
 end

da86a17f6b2792f46313666c838bf7204f24201c
Eric <ebeecroft@mail.csuchico.edu>
2016-05-15 08:02:42 -0700

    Shutting down all the bad routes

--- Trial/config/routes.rb
+++ Trial/config/routes.rb
@@ -1,31 +1,28 @@
 Trial::Application.routes.draw do
 
-  resources :usertypes
-
-
    #Builds the users actions and the nested actions
    get '/users/maintenance' => 'users#maintenance'
-   resources :users, :except => [:new] do
+   resources :users, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #builds everything except new
 #      get '/:id/page/:page', :action => :index, :on => :collection
-      resources :petowners
-      resources :inventories, :only =>[:index, :create, :destroy]
-      resources :comments, :only => [:create, :destroy]
-      resources :forums
-      resources :sbooks
-      resources :mainfolders
+      resources :petowners, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything
+      resources :inventories, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #:only =>[:index, :create, :destroy] #Builds only index, create, destroy
+      resources :comments, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #:only => [:create, :destroy] #Builds only create and destroy
+      resources :forums, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything 
+      resources :sbooks, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything
+      resources :mainfolders, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything
    end
 
    #Builds the mainfolders
    get '/mainfolders/maintenance' => 'mainfolders#maintenance'
    get 'mainfolders/list' => 'mainfolders#list'
-   resources :mainfolders, :only => [:index] do
-      resources :subfolders, :except =>[:index]
+   resources :mainfolders, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :subfolders, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Everything except index
    end
 
    #Builds the subfolders
    get '/subfolders/maintenance' => 'subfolders#maintenance'
-   resources :subfolders, :only =>[:index] do
-      resources :artworks, :except =>[:index]
+   resources :subfolders, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :artworks, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Everything except index
    end
 
    #Builds the artworks
@@ -33,20 +30,20 @@ Trial::Application.routes.draw do
    get '/artworks/review' => 'artworks#review' #has to be before the pets controller
    post 'artworks/review1' => 'artworks#approve'
    post 'artworks/review2' => 'artworks#deny'
-   resources :artworks, :only => [:index]
+   resources :artworks, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Builds the series
    get '/sbooks/maintenance' => 'sbooks#maintenance'
    get 'sbooks/list' => 'sbooks#list'
-   resources :sbooks, :only => [:index] do
-      resources :books, :except =>[:index]
+   resources :sbooks, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :books, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Everything except index
    end
 #get '/:id/page/:page', :action => :show, :on => :collection
 
    #Builds the books
    get '/books/maintenance' => 'books#maintenance'
-   resources :books, :only => [:index] do
-      resources :chapters, :except => [:index, :show]
+   resources :books, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :chapters, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #everything except index and show
    end
 
    #Builds the chapters
@@ -54,21 +51,21 @@ Trial::Application.routes.draw do
    get 'chapters/review' => 'chapters#review'
    post 'chapters/review1' => 'chapters#approve'
    post 'chapters/review2' => 'chapters#deny'
-   resources :chapters, :only => [:index]
-   resources :gchapters, :only =>[:index]
+   resources :chapters, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
+   resources :gchapters, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Comments actions
-   resources :comments, :only =>[:index]
+   resources :comments, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Suggestion box for users
-   resources :suggestions, :except =>[:edit, :update, :show]
+   resources :suggestions, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Everything except edit, update, and show
 
    #Inventory routes
    get '/inventories/maintenance' => 'inventories#maintenance'
-   resources :inventories, :only =>[:index]
+   resources :inventories, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Money Bag
-   resources :pouches, :only => [:index]
+   resources :pouches, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Builds the pet actions
    get '/pets/maintenance' => 'pets#maintenance'
@@ -81,70 +78,81 @@ Trial::Application.routes.draw do
    post 'pets/review2' => 'pets#deny'
 
    #Base pets route
-   resources :pets
+   resources :pets, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything for pets
 
    #Builds the item actions
    get '/items/maintenance' => 'items#maintenance'
-   resources :items
+   resources :items, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything for items
 
    #Builds the equips and fights actions
+   post 'petowners/increase1' => 'petowners#health'
+   post 'petowners/increase2' => 'petowners#attack'
+   post 'petowners/increase3' => 'petowners#defense'
+   post 'petowners/increase4' => 'petowners#speed'
    get '/petowners/maintenance' => 'petowners#maintenance'
-   resources :petowners, :only =>[:index] do #Prevents building the petowners routes
-      resources :equips, :only => [:index, :create, :destroy]
-      resources :fights, :except => [:new, :edit, :update]
+   resources :petowners, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Prevents building the petowners routes #Only index
+      resources :equips, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only builds index, create and destroy
+      resources :fights, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything except new, edit, and update
    end
 
    #Builds fights path
    get '/fights/maintenance' => 'fights#maintenance'
    post '/petowners/:petowner_id/fights/:id' => 'fights#battle'
-   resources :fights, :only =>[:index]
+   resources :fights, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Builds equips path
    get '/equips/maintenance' => 'equips#maintenance'
-   resources :equips, :only =>[:index]
+   resources :equips, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Builds the forum index page and nested routes
    get '/forums/maintenance' => 'forums#maintenance'
    get '/forums/list' => 'forums#list'
-   resources :forums, :only =>[:index] do
-      resources :tcontainers, :except =>[:index]
+   resources :forums, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :tcontainers, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything but index
    end
 
    #Builds the container for the topics and the nested routes
    get '/tcontainers/maintenance' => 'tcontainers#maintenance'
-   resources :tcontainers, :only =>[:index] do
-      resources :maintopics, :except => [:index]
+   resources :tcontainers, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :maintopics, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything except index
    end
 
    #Build the forum maintopics actions and the nested actions
    get '/maintopics/maintenance' => 'maintopics#maintenance'
-   resources :maintopics, :only =>[:index] do
-      resources :subtopics, :except => [:index]
+   resources :maintopics, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Only index
+      resources :subtopics, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything except index
    end
 
    #Builds the forum narratives actions
    get '/subtopics/maintenance' => 'subtopics#maintenance'
-   resources :subtopics, :only =>[:index] do #Prevents building the subtopics routes
-      resources :narratives, :except => [:show, :index]
+   resources :subtopics, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] do #Prevents building the subtopics routes #Only index
+      resources :narratives, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything except index and show
    end
 
    #Builds the narrative routes
    get '/narratives/maintenance' => 'narratives#maintenance'
-   resources :narratives, :only => [:index]
+   resources :narratives, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
 
    #Creates the user connection to the website
-   resources :sessions, :only => [:create]
+   resources :sessions, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only create
+
+   #Creates the recovery routes
+   resources :passwordrecoveries, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only create
 
    #Login pages + user signup
+   get '/recover' => 'passwordrecoveries#new'
    get '/signup' => 'users#new'
    get '/signin' => 'sessions#new'
    match '/logout' => 'sessions#destroy', via: :delete #has to be a match condition
 
    #Builds the sessionkey
-   resources :sessionkeys, :only => [:index]
+   resources :sessionkeys, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only index
+
+   #Builds the staff
+   resources :usertypes, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Only builds index, new, create, edit and update actions
 
    #Controls page visibility
-   resources :maintenancemodes, :except =>[:show, :destroy]
+   resources :maintenancemodes, :except =>[:index, :show, :new, :create, :edit, :update, :destroy] #Builds everything except show and destroy
 
    #Root pages
    get 'maintenance' => "start#maintenance"
@@ -154,4 +162,3 @@ Trial::Application.routes.draw do
    post 'create_adoption', to: "petowners#create"
    root :to => "start#home"
 end
-
